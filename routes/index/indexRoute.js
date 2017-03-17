97bd0e67e35942bc9b0f913b552439a0f68def21
Lemon9412 <18431164602@163.com>
2017-03-17 12:29:08 +0800

    1111

--- routes/index/indexRoute.js
+++ routes/index/indexRoute.js
@@ -3,12 +3,9 @@
  */
 var express = require("express");
 var router = express.Router();
-var dao = require("./../../dao/indexDao.js");
-var co = require("co");
 
 
-router.post("/index",function(req,resp) {
-    co(function *() {
+router.post("/",function(req,resp) {
         var photo = req.body.photo;
         var name = req.body.name;
         var male = req.body.male;
@@ -19,19 +16,19 @@ router.post("/index",function(req,resp) {
         var cerName = req.body.cerName;
         var cerday = req.body.cerday;
         var company = req.body.company;
-
-        var info = yield dao.findInfoByIDcard(IDcard);
-        console.log("qiqi");
-        if(info!=null) {
-            resp.send({state:1});
-        } else {
-            yield dao.insertInfo({photo:photo,name:name,male:male,female:female,birthday:birthday,num:num,IDcard:IDcard,cerName:cerName,cerday:cerday,company:company});
-            var curInfo = yield dao.findInfoByIDcard(IDcard);
-            req.session.curInfo = info;
-            resp.send({state:2});
-        }
-    }).catch(function(e) {
-        resp.send({state:0});
-    });
+        var info = {
+            photo:photo,
+            name:name,
+            male:male,
+            female:female,
+            birthday:birthday,
+            num:num,
+            IDcard:IDcard,
+            cerName:cerName,
+            cerday:cerday,
+            company:company
+        };
+        var colletion = database.collection("users");
+        colletion.insert(info);
 });
 module.exports = router;
