be03d548d61dc8c1b46952ae77df99bb55638970
GeniusLiu <48235303@qq.com>
2017-03-21 11:38:49 +0800

    test

--- routes/describe/describeRoute.js
+++ routes/describe/describeRoute.js
@@ -4,10 +4,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.post("/",function(req,resp) {
+router.post("/", function (req, resp) {
     var user_id = req.body.user_id;
     var collection = database.collection("users");
-    var data = collection.find({_id:ObjectID(user_id)}).limit(1).next();
-    resp.send(data);
+    collection.find({_id: ObjectID(user_id)}).limit(1).toArray(function (err, data) {
+        console.log(data);
+        resp.send(data);
+    });
 });
-module.exports=router;
+module.exports = router;
